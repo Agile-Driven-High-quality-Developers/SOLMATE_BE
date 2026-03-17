5479ef7e0d7c9a53a774a4a0940c9a33d7392417
shinyj0 <yejins0605@gmail.com>
2026-03-17 13:14:07 +0900

    init

---
+++ src/main/java/org/solmate/SolmateApplication.java
@@ -0,0 +1,13 @@
+package org.solmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/solmate/SolmateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

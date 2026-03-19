c49a3f5ab5ecc12b431f9e08e3baf8681a1e8a79
shinyj0 <yejins0605@gmail.com>
2026-03-19 10:49:20 +0900

    refactor: stock 엔티티 변경

---
+++ src/main/java/org/solmate/domain/stock/enums/SectorType.java
@@ -0,0 +1,15 @@
+package org.solmate.domain.stock.enums;
+
+public enum SectorType {
+	INFORMATION_TECHNOLOGY, // 정보기술(IT)
+	FINANCIALS,             // 금융
+	ENERGY_CHEMICALS,       // 에너지/화학
+	STEEL_MATERIALS,        // 철강/소재
+	CONSUMER_DISCRETIONARY, // 경기소비재
+	CONSUMER_STAPLES,       // 생활소비재
+	INDUSTRIALS,            // 산업재
+	HEAVY_INDUSTRIES,       // 중공업
+	CONSTRUCTION,           // 건설
+	HEALTHCARE              // 헬스케어
+
+}

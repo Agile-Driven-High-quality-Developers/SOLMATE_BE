22b3eb6dc48edba9434441364c0b7c17ffd9dfdf
shinyj0 <yejins0605@gmail.com>
2026-03-21 14:51:51 +0900

    refactor: enum값 EXECUTED에서 FILLED으로 변경

--- src/main/java/org/solmate/domain/trade/enums/TradeType.java
+++ src/main/java/org/solmate/domain/trade/enums/TradeType.java
@@ -1,6 +1,13 @@
 package org.solmate.domain.trade.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TradeType {
-    BUY,
-    SELL
+    BUY("매수"),
+    SELL("매도");
+
+    private final String label;
 }

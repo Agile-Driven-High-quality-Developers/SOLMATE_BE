1dcdddea5bbf0a7c1c81f105c589626f6b46455b
shinyj0 <yejins0605@gmail.com>
2026-03-21 14:50:10 +0900

    refactor: 로그인 dto에서 스웨거 기본 값 설정

--- src/main/java/org/solmate/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/org/solmate/domain/auth/dto/request/LoginRequest.java
@@ -1,15 +1,18 @@
 package org.solmate.domain.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
+        @Schema(example = "somate2026@gmail.com")
         @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         @Size(max = 30, message = "이메일은 30자를 초과할 수 없습니다.")
         String email,
 
+        @Schema(example = "string1234")
         @NotBlank(message = "비밀번호는 필수입니다.")
         @Size(min = 8, message = "비밀번호는 최소 8자 이상입니다.")
         String password

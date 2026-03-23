5cc41a7f01a4f382dd43c759909d1c59f8236030
uoo6uoow <154813925+uoo6uoow@users.noreply.github.com>
2026-03-23 10:52:24 +0900

    feat: t8412/t8410 API 클라이언트 추가

--- src/main/java/org/solmate/external/ls/client/LsApiClient.java
+++ src/main/java/org/solmate/external/ls/client/LsApiClient.java
@@ -1,7 +1,11 @@
 package org.solmate.external.ls.client;
 
 import org.solmate.external.ls.LsProperties;
+import org.solmate.external.ls.dto.request.LsDailyCandleRequest;
+import org.solmate.external.ls.dto.request.LsMinuteCandleRequest;
 import org.solmate.external.ls.dto.request.LsQuoteRequest;
+import org.solmate.external.ls.dto.response.LsDailyCandleResponse;
+import org.solmate.external.ls.dto.response.LsMinuteCandleResponse;
 import org.solmate.external.ls.dto.response.LsQuoteResponse;
 import org.solmate.external.ls.service.LsTokenService;
 import org.springframework.http.MediaType;
@@ -28,5 +32,61 @@ public class LsApiClient {
                 .body(LsQuoteRequest.of(stockCode))
                 .retrieve()
                 .body(LsQuoteResponse.class);
+    }
+
+    /** t8412: 1분봉 조회 (처음 조회) */
+    public LsMinuteCandleResponse getMinuteCandles(String stockCode, String sdate, String edate) {
+        return restClient.post()
+                .uri(lsProperties.getBaseUrl() + "/stock/chart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + lsTokenService.getToken())
+                .header("tr_cd", "t8412")
+                .header("tr_cont", "N")
+                .body(LsMinuteCandleRequest.of(stockCode, sdate, edate))
+                .retrieve()
+                .body(LsMinuteCandleResponse.class);
+    }
+
+    /** t8412: 1분봉 연속 조회 */
+    public LsMinuteCandleResponse getMinuteCandlesContinue(String stockCode, String sdate, String edate,
+                                                            String ctsDate, String ctsTime) {
+        return restClient.post()
+                .uri(lsProperties.getBaseUrl() + "/stock/chart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + lsTokenService.getToken())
+                .header("tr_cd", "t8412")
+                .header("tr_cont", "Y")
+                .header("tr_cont_key", ctsDate + ctsTime)
+                .body(LsMinuteCandleRequest.ofContinue(stockCode, sdate, edate, ctsDate, ctsTime))
+                .retrieve()
+                .body(LsMinuteCandleResponse.class);
+    }
+
+    /** t8410: 일봉 조회 (처음 조회) */
+    public LsDailyCandleResponse getDailyCandles(String stockCode, String sdate, String edate) {
+        return restClient.post()
+                .uri(lsProperties.getBaseUrl() + "/stock/chart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + lsTokenService.getToken())
+                .header("tr_cd", "t8410")
+                .header("tr_cont", "N")
+                .body(LsDailyCandleRequest.of(stockCode, sdate, edate))
+                .retrieve()
+                .body(LsDailyCandleResponse.class);
+    }
+
+    /** t8410: 일봉 연속 조회 */
+    public LsDailyCandleResponse getDailyCandlesContinue(String stockCode, String sdate, String edate,
+                                                          String ctsDate) {
+        return restClient.post()
+                .uri(lsProperties.getBaseUrl() + "/stock/chart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + lsTokenService.getToken())
+                .header("tr_cd", "t8410")
+                .header("tr_cont", "Y")
+                .header("tr_cont_key", ctsDate)
+                .body(LsDailyCandleRequest.ofContinue(stockCode, sdate, edate, ctsDate))
+                .retrieve()
+                .body(LsDailyCandleResponse.class);
     }
 }

50a7e5000fe63be966a385c2e774612417e48a9e
shinyj0 <yejins0605@gmail.com>
2026-03-25 09:52:56 +0900

    feat: 시가총액 수동 업데이트 구현

---
+++ src/main/java/org/solmate/domain/stock/controller/StockTotalController.java
@@ -0,0 +1,29 @@
+package org.solmate.domain.stock.controller;
+
+import org.solmate.common.response.ApiResponse;
+import org.solmate.common.status.SuccessStatus;
+import org.solmate.domain.stock.service.StockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Admin Stock", description = "주식 관리자 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/stocks")
+public class StockTotalController {
+
+    private final StockService stockService;
+
+    @Operation(summary = "시가총액 수동 업데이트", description = "전체 종목의 시가총액을 수동으로 업데이트합니다.")
+    @PostMapping("/market-cap/update")
+    public ResponseEntity<ApiResponse<Void>> updateMarketCap() {
+        stockService.updateAllMarketCap();
+        return ApiResponse.success(SuccessStatus.SUCCESS_204);
+    }
+}
